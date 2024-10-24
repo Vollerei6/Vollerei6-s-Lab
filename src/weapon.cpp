ce90c238d581cc9f74e29c6fce7f356c36527bef
why <3480874353@qq.com>
2024-10-24 13:21:39 +0800

    save weapon

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -1,5 +1,7 @@
 #include "weapon.hpp"
 #include <string>
+#include "nlohmann/json.hpp"
+#include <fstream>
 
 Weapon::Weapon(const std::string& name, double ATK, double Crit, double combo, double suck)
 {
@@ -7,4 +9,12 @@ Weapon::Weapon(const std::string& name, double ATK, double Crit, double combo, d
 	this->ATK = ATK;
 	this->Crit = Crit;
 	this->combo = combo;
+}
+
+void Weapon::Save(const std::string& characterName,nlohmann::json &archive)
+{
+	archive[characterName]["weapon"][this->name]["ATK"] = this->ATK;
+	archive[characterName]["weapon"][this->name]["Crit"] = this->Crit;
+	archive[characterName]["weapon"][this->name]["combo"] = this->combo;
+	archive[characterName]["weapon"][this->name]["suck"] = this->suck;
 }
